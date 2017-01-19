3244b3de08b2b673120f9c4afd0529916cbf86f1
dujiahui(杜佳桧.商城技术部.前端研发部) <dujiahui@ds.gome.com.cn>
2017-01-19 12:14:58 +0800

    预约购买和添加购物车跳转

--- src/scripts/about/product.js
+++ src/scripts/about/product.js
@@ -1,7 +1,8 @@
+/*加入购物车*/
 $("#product-box").delegate(".addTo-cart", "click", function() {
     var $info = $(this).parents(".product-item").find(".productInfo");
     var _type = 0;
-
+    var infoBox = $("#item-info-body");
     if($info.attr("isHyg") == "1"){_type = 16};
     if($info.attr("isTaogou") == "true"){_type = 24};
     /* 加入购物车美通卡入口*/
@@ -12,12 +13,27 @@ $("#product-box").delegate(".addTo-cart", "click", function() {
     }
 });
 
-//添加收藏
+
+
+/*添加收藏*/
 $("#product-box").delegate(".add-collection", "click", function() {
     g.login(function(){
         //searchBase.addCollection($productInfoInput.attr("pId"),$productInfoInput.attr("sId"), loginData.loginId, "wishlist", $productInfoInput.attr("pName"));
-        require('../function/addCollection').addCollect($productInfoInput.attr("pId"),$productInfoInput.attr("sId"),loginData.loginId,$productInfoInput.attr("pName"),"wishlist");
+        require('../function/addCollection').addCollect("9134521004","1123461018",loginData.loginId,"华为 HUAWEI Mate 9 4GB+32GB 全网通版 月光银","wishlist");
 
     });
 });
+
+/*到货通知*/
+$("#product-box").delegate(".next-buy","click",function(){
+    var request_tre = function(){};
+    var content = '<div class="dh-warp"><h3 class="dh-title">到货通知</h3><p class="dh-info">一旦该商品到货，我们会通过手机短信或邮件通知您</p><table class="dh-form"><tbody><tr><td class="dh-hd"><em class="nHeigh">*</em>手机号码：</td><td><input class="dh-input-text" id="dh-telNum" type="text"><span id="dh-telNum-warn"></span></td></tr><tr><td class="dh-hd"><em class="nHeigh">*</em>邮箱地址：</td><td><input class="dh-input-text" id="dh-email" type="text"><span id="dh-email-warn"></span></td></tr><tr><td>&nbsp;</td><td class="dh-label-box"><label class="gmform-label" for="dhAddCollection"><input class="gmform-input-check" name="dhAddCollection" id="dhAddCollection" type="checkbox">同时加入收藏夹</label></td></tr><tr><td>&nbsp;</td><td class="dh-btn-box"><a href="javascript:void(0)" class="dh-btn-submite" id="dh-submite">确定</a><a href="javascript:void(0)" class="dh-btn-cancel closeBtn">取消</a></td></tr></tbody></table></div>';
+    require('../function/showMask').showMask(content,request_tre);
+    $("#mask-overlay").remove();
+
+
+})
+
+
+
 
--- src/scripts/function/showMask.js
+++ src/scripts/function/showMask.js
@@ -11,21 +11,41 @@ define(function(require,exports,module){
 	        $windowHeight = $(window).height(),
 	        $h = Math.max($bodyHeight,$windowHeight);
         //弹出层主内容
-        var $maskContentWarp = $('<div class="mask-box" id="mask-box"><a class="mask-close closeBtn" href="javascript:void(0);">╳</a><div class="mask-content-warp" id="mask-content-warp">'+content+'</div></div>'),
-        	$contentHeight = $maskContentWarp.height(),
-        	$contentWidth = $maskContentWarp.width()
+        var $maskContentWarp = $('<div class="mask-box" id="mask-box"><a class="mask-close closeBtn" href="javascript:void(0);">╳</a><div class="mask-content-warp" id="mask-content-warp">'+content+'</div></div>');
+        $maskContentWarp.appendTo('body');
+        var $contentHeight = $maskContentWarp.height(),
+        	$contentWidth = $maskContentWarp.width();
+
 
         $('<div id="mask-overlay" class="mask-overlay"></div>').css("height",$h).appendTo('body');
         $maskContentWarp.css({
             "margin-left":-($contentWidth/2),
             "margin-top":-($contentHeight/2)
-        }).appendTo('body');
-
+        });
         if(callback && typeof callback == "function"){
             callback.apply();
         }
         $(".closeBtn").on('click',function(){
             closeMask();
+        })
+        $(".dh-btn-submite").on('click',function(){
+            var a = $("#dhAddCollection").attr("checked");
+            var collect = "";
+            var cookieAg = $.cookie("atgregion").split("|")[0];
+            var userId = $.cookie("SSO_USER_ID");
+            require('../function/addCart').check_phone($("#dh-telNum").val(),$("#dh-telNum-warn"));
+            if(require('../function/addCart').check_phone($("#dh-telNum").val(),$("#dh-telNum-warn"))== true){
+                require('../function/addCart').check_email($("#dh-email").val(),$("#dh-email-warn"));
+                if(require('../function/addCart').check_email($("#dh-email").val(),$("#dh-email-warn")) == true){
+                    if(a=="checked"){
+                        collect = true;
+                    }else{
+                        collect = false;
+                    }
+                    require('../function/addCart').arriveNotice("9134521004","1123461018",userId,cookieAg,$("#dh-telNum").val(),$("#dh-email").val(),collect);
+                    closeMask();
+                }
+            }
         })
 	}
 
